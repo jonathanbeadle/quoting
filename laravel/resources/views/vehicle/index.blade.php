e81b2c209eb7f1c7bec5b0e0a06cf78862dd61c4
JB <jonny_beadle@hotmail.com>
2025-03-14 14:03:19 +0000

    Pagination

--- laravel/resources/views/vehicle/index.blade.php
+++ laravel/resources/views/vehicle/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between align-items-center mb-3">
-        <h1>All Vehicles</h1>
+        <h1>All Vehicles ({{ $vehicles->total() }})</h1>
         <div class="d-flex align-items-center">
             {{ $vehicles->links() }}
             <form action="{{ route('vehicle.index') }}" method="GET" class="d-flex ms-3">
@@ -17,41 +17,42 @@
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
     
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Make &amp; Model</th>
-                <th>Specification</th>
-                <th>Transmission</th>
-                <th>Fuel Type</th>
-                <th>Registration Status</th>
-                <th>Colour</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($vehicles as $vehicle)
-            <tr>
-                <td>{{ $vehicle->id }}</td>
-                <td>{{ $vehicle->make }} {{ $vehicle->model }}</td>
-                <td>{{ $vehicle->specification }}</td>
-                <td>{{ $vehicle->transmission }}</td>
-                <td>{{ $vehicle->fuel_type }}</td>
-                <td>{{ $vehicle->registration_status }}</td>
-                <td>{{ $vehicle->colour }}</td>
-                <td>
-                    <a href="{{ route('vehicle.show', ['id' => $vehicle->id]) }}" class="btn btn-sm btn-info">View</a>
-                    <a href="{{ route('vehicle.edit', ['id' => $vehicle->id]) }}" class="btn btn-sm btn-warning">Edit</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped align-middle">
+            <thead>
+                <tr>
+                    <th style="width: 5%">ID</th>
+                    <th style="width: 15%">Make & Model</th>
+                    <th style="width: 20%">Specification</th>
+                    <th style="width: 10%">Transmission</th>
+                    <th style="width: 10%">Fuel Type</th>
+                    <th style="width: 15%">Registration Status</th>
+                    <th style="width: 10%">Colour</th>
+                    <th style="width: 15%">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($vehicles as $vehicle)
+                <tr>
+                    <td>{{ $vehicle->id }}</td>
+                    <td class="text-nowrap">{{ $vehicle->make }} {{ $vehicle->model }}</td>
+                    <td class="text-nowrap">{{ $vehicle->specification }}</td>
+                    <td class="text-nowrap">{{ $vehicle->transmission }}</td>
+                    <td class="text-nowrap">{{ $vehicle->fuel_type }}</td>
+                    <td class="text-nowrap">{{ $vehicle->registration_status }}</td>
+                    <td class="text-nowrap">{{ $vehicle->colour }}</td>
+                    <td class="text-nowrap">
+                        <a href="{{ route('vehicle.show', ['id' => $vehicle->id]) }}" class="btn btn-sm btn-info">View</a>
+                        <a href="{{ route('vehicle.edit', ['id' => $vehicle->id]) }}" class="btn btn-sm btn-warning">Edit</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     
-    <!-- Pagination Links -->
-    <div class="d-flex justify-content-center mt-3">
-        {{ $vehicles->links() }}
+    <div class="mt-3">
+        <a href="{{ route('vehicle.create') }}" class="btn btn-primary">Create New Vehicle</a>
     </div>
 </div>
 @endsection
